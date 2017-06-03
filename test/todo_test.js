c1799997b76673b66ed7cfdda826092351194a1c
0phoff <0phoff@users.noreply.github.com>
2017-06-03 20:41:31 +0200

    Added functionality to List
    Added the List#removeItem() and List#findItems() functions :punch:

--- test/todo_test.js
+++ test/todo_test.js
@@ -1,86 +1,134 @@
 const assert = require('assert');
 const todo = require('../lib/todo');
 
-describe('todo.js', function() {
-  describe('List', function() {
-    let list;
+context('todo.js', function() {
+  describe('Class List', function() {
     beforeEach(function() {
-      list = new todo.List('rad\n#title!', '###sanitizing\n\nis really nice\n  - 1\n* 2')
+      this.list = new todo.List('rad\n#title!', '###sanitizing\n\nis really nice\n  - 1\n* 2')
     });
 
     describe('#ctor', function() {
       it('should return a valid & sanitized object', function() {
-        assert.equal(list.title, ' rad #title!');
-        assert.equal(list.desc, 'sanitizing\nis really nice');
-        assert.equal(list.length, 0);
-        assert.equal(list.done, 0);
-        assert.deepEqual(list._content, []);
+        assert.equal(this.list.title, ' rad #title!');
+        assert.equal(this.list.desc, 'sanitizing\nis really nice');
+        assert.equal(this.list.length, 0);
+        assert.equal(this.list.done, 0);
+        assert.deepEqual(this.list._content, []);
       });
     });
 
-    describe('#addItem', function() {
+    describe('#insertItem', function() {
       it('should add item objects', function() {
         let item = {value: 'item', done: false};
-        let index = list.addItem(list.length, 'item');
+        let index = this.list.insertItem('item');
 
-        assert.equal(list.length, 1);
-        assert.deepEqual(list.getItem(index), item);
+        assert.equal(this.list.length, 1);
+        assert.deepEqual(this.list.getItem(index), item);
 
-        list.addItem(0, 123, true);
-        assert.deepEqual(list.getItem(0), { value: '123', done: true });
+        this.list.insertItem(123, true, 0);
+        assert.deepEqual(this.list.getItem(0), { value: '123', done: true });
+      });
+    });
+
+    describe('#removeItem', function() {
+      it('should remove item objects', function() {
+        this.list.insertItem('item1');
+        this.list.insertItem('item2');
+        this.list.insertItem('item3');
+
+        this.list.removeItem(1);
+        assert.equal(this.list.length, 2);
+        this.list.removeItem(1);
+        assert.equal(this.list.length, 1);
+        assert.equal(this.list.getItem(0).value, 'item1');
+        this.list.removeItem(0);
+        assert.equal(this.list.length, 0);
       });
     });
 
     describe('#changeItem', function() {
       it('should change the right object', function() {
-        list.addItem(0, 'item1');
-        list.addItem(1, 'item2');
-        list.changeItem(0, 'item0');
+        this.list.insertItem('item1');
+        this.list.insertItem('item2');
+        this.list.changeItem(0, 'item0');
 
-        assert.deepEqual(list.getItem(0), {value:'item0', done:false});
-        assert.deepEqual(list.getItem(1), {value:'item2', done:false});
+        assert.deepEqual(this.list.getItem(0), {value:'item0', done:false});
+        assert.deepEqual(this.list.getItem(1), {value:'item2', done:false});
       });
     });
 
     describe('#markItem', function() {
-      it('should mark the item as done', function() {
-        list.addItem(0, 'item1');
-        list.addItem(1, 'item2', true);
-        list.markItem(0, true);
-        list.markItem(1, false);
+      it('should mark an item as done', function() {
+        this.list.insertItem('item');
+        this.list.markItem(0, true);
 
-        assert(list.getItem(0).done);
-        assert(!list.getItem(1).done);
+        assert(this.list.getItem(0).done);
+      });
+
+      it('should mark an item as todo', function() {
+        this.list.insertItem('item', true);
+        this.list.markItem(0, false);
+
+        assert(!this.list.getItem(0).done);
       });
     });
 
     describe('#toggleItem', function() {
       it('should toggle the state of an item', function() {
-        list.addItem(0, 'item1');
-        list.addItem(1, 'item2');
-        list.markItem(1, true);
+        this.list.insertItem('item0');
+        this.list.insertItem('item1');
+        this.list.markItem(1, true);
 
-        list.toggleItem(0);
-        assert(list.getItem(0).done);
-        list.toggleItem(1);
-        assert(!list.getItem(1).done);
+        this.list.toggleItem(0);
+        assert(this.list.getItem(0).done);
+        this.list.toggleItem(1);
+        assert(!this.list.getItem(1).done);
+      });
+    });
+
+    describe('#findItems', function() {
+      it('should return an array of matched items', function() {
+        this.list.insertItem('it is beautiful');
+        this.list.insertItem('item1');
+        this.list.insertItem('no bueno');
+        this.list.insertItem('the beacons are lit!');
+
+        assert.deepEqual(this.list.findItems('it'), [0,1,3]);
+      });
+
+      it('should be smart-case sensitive', function() {
+        this.list.insertItem('item');
+        this.list.insertItem('Italiano');
+
+        assert.deepEqual(this.list.findItems('it'), [0,1])
+        assert.deepEqual(this.list.findItems('It'), [1]);
+      });
+
+      it('should accept regular expresions', function() {
+        this.list.insertItem('Italiano');
+        this.list.insertItem('The beacons are lit! Are not?');
+        
+        assert.deepEqual(this.list.findItems('it.*no'), [0,1]);
       });
     });
   });
 
-  describe('Project', function() {
-    let project;
+  describe.skip('Class Project', function() {
     beforeEach(function() {
-      project = new todo.Project('rad\n#title!', '###sanitizing\n\nis really nice\n  - 1\n* 2');
+      this.project = new todo.Project(' #title', 'description\nmultiline!');
     });
 
     describe('#ctor', function() {
       it('should return a valid & sanitized object', function() {
-        assert.equal(project.title, ' rad #title!');
-        assert.equal(project.desc, 'sanitizing\nis really nice');
-        assert.equal(project.length, 0);
-        assert.deepEqual(project._content, []);
+        assert.equal(this.project.title, ' #title');
+        assert.equal(this.project.desc, 'description\nmultiline!');
+        assert.equal(this.project.length, 0);
+        assert.deepEqual(this.project._content, []);
       });
+    });
+
+    describe.skip('', function() {
+      
     });
   });
 });
