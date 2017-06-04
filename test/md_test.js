df858267f649c1d18e5c604404236ea9f9c710ec
0phoff <0phoff@users.noreply.github.com>
2017-06-04 19:24:28 +0200

    Cleaned up tests for md.js

--- test/md_test.js
+++ test/md_test.js
@@ -1,6 +1,5 @@
 const assert = require('assert');
 const fs = require('fs');
-// TODO: const common = require('./common');
 const md = require('../lib/md');
 const todo = require('../lib/todo');
 
@@ -10,6 +9,7 @@ describe('md.js', function() {
     it('should return a promise', function() {
       let p = md.readFile('file');
       assert(p instanceof Promise);
+      return p.catch(err => Promise.resolve(err));
     });
 
     it('should reject the promise with a non-existent filename', function() {
@@ -18,31 +18,20 @@ describe('md.js', function() {
     });
 
     it('shoud reject the promise with a file that has a syntax error', function() {
-      fs.writeFileSync('invalid.md','#title\ndescription\n\n#NoH2\ndescription\n  - [ ] item');
+      fs.writeFileSync('invalid.md','#invalid\nproject desc\n\n#NoH2\nlist desc\n  - [ ] item');
       let p = md.readFile('invalid.md');
-      return p
-        .then(success => {
-          fs.unlinkSync('invalid.md');
-          return Promise.reject(success);
-        }, err => {
-          fs.unlinkSync('invalid.md');
-          return Promise.resolve();
-        });
+      return p.then(succes => Promise.reject(succes), err => Promise.resolve(err));
     });
 
     it('should resolve the promise with a valid file', function() {
-      fs.writeFileSync('valid.md', '#title\ndescription\n\n## H2 here\ndescription\n  - [ ] item\n- [X] item\n');
+      fs.writeFileSync('valid.md', '#valid\nproject desc\n\n## H2 here\nlist desc\n  - [ ] item\n- [X] item\n');
       let p = md.readFile('valid.md');
 
       return p
         .then(data => {
-          fs.unlinkSync('valid.md');
           assert(data instanceof todo.Project);
           return Promise.resolve();
-        }, err => {
-          fs.unlinkSync('valid.md');
-          return Promise.reject(err);
-        });
+        }, err => Promise.reject(err));
     });
 
     after(function() {
@@ -59,6 +48,7 @@ describe('md.js', function() {
     it('should return a promise', function() {
       let p = md.writeFile('file', {});
       assert(p instanceof Promise);
+      return p.catch(err => Promise.resolve(err));
     });
     
     it('should reject the promise with an invalid object', function() {
@@ -68,13 +58,16 @@ describe('md.js', function() {
 
     it('should resolve the promise with a valid object', function() {
       let todoObj = new todo.Project('title', 'description');
-      let p = md.writeFile('file', todoObj);
+      todoObj.insertList(new todo.List('list1', 'description'));
+      todoObj.getList(0).insertItem('todo item');
+      todoObj.getList(0).insertItem('done item', true);
+      let p = md.writeFile('file.md', todoObj);
 
       return p
-        .then(filename => {
-          let file = fs.readFileSync(filename, 'utf8');
-          assert.equal(file, '# title\ndescription');
-          fs.unlinkSync(filename);
+        .then(() => {
+          let file = fs.readFileSync('file.md', 'utf8');
+          fs.unlinkSync('file.md');
+          assert.equal(file, '# title\ndescription\n\n## list1\ndescription\n  - [ ] todo item\n  - [X] done item\n\n');
           return Promise.resolve();
         });
     });
